c67d50e881c2801e696a62582cdf457f6e6a3367
mert1996 <merttekin0@gmail.com>
2025-04-28 01:37:19 +0300

    Problem download from url is available

--- src/tsp_instance.py
+++ src/tsp_instance.py
@@ -3,8 +3,13 @@ import numpy as np
 
 
 class TSPInstance:
-    def __init__(self, problem_codename):
-        self.problem = tsplib95.load(f'./data/tsp/{problem_codename}.tsp')
+    def __init__(self, problem_codename=None, URL=None):
+
+        if problem_codename is not None:
+            self.problem = tsplib95.load(f'./data/tsp/{problem_codename}.tsp')
+        else:
+            self.problem = tsplib95.load(URL)
+
         if "node_coords" in self.problem.as_name_dict().keys():
             coords = self.problem.as_name_dict()["node_coords"]
             self.nodes = {i: coords[i] for i in sorted(coords)}
@@ -17,5 +22,5 @@ class TSPInstance:
         dm = np.zeros((self.n, self.n))
         for i, (ix, iy) in self.nodes.items():
             for j, (jx,jy) in self.nodes.items():
-                dm[i-1,j-1] = np.hypot(ix-jx, iy-jy)
+                dm[i-1, j-1] = np.hypot(ix-jx, iy-jy)
         return dm
